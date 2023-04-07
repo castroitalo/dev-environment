8d239cd06abcf16285eb782afecbecda8d7b5546
Italo Castro <dev.castro.italo@gmail.com>
2023-04-07 01:51:59 -0300

    correct file added

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -126,20 +126,8 @@
 (use-package ace-window
   :ensure t
   :bind (("C-x o" . ace-window)))
+  
+;; THEME 
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'zenburn)
 
-;; MELPA GENERATED STUFF
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
- '(package-selected-packages
-   '(org-bullets counsel-etags flycheck which-key wich-key auto-complete use-package php-mode magit ivy evil emmet-mode auto-indent-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
