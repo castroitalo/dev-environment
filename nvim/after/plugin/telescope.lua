5c2d1f5cb63ff465c6b7f0b6d11517ec40971086
Italo Castro <102182915+devcastroitalo@users.noreply.github.com>
2023-08-07 09:46:53 -0300

    Update telescope.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,2 +1,12 @@
 local builtin = require('telescope.builtin')
+local telescope = require ('telescope')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+telescope.setup {
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    }
+}
